bc5311defdfacd5f31c507ae6ea155fac2e2d0bc
Damiano Oldoni <damiano.oldoni@inbo.be>
2022-10-14 16:57:27 +0200

    Move occurrenceRemarks after occurrenceStatus

--- sql/dwc_occurrence.sql
+++ sql/dwc_occurrence.sql
@@ -8,7 +8,6 @@ SELECT
   'HumanObservation'                    AS basisOfRecord,
 -- OCCURRENCE
   o."location" || ':' || o."datum" || ':' || o."species_name_hash" AS occurrenceID,
-  o."opmerking"                         AS occurrenceRemarks,
   CASE
     WHEN o."n" > 0  THEN o."n"
     ELSE NULL
@@ -17,6 +16,7 @@ SELECT
     WHEN o."n" = 0  THEN "absent"
     ELSE "present"
   END                                   AS occurrenceStatus,
+  o."opmerking"                         AS occurrenceRemarks,
 -- TAXON
   o."species"                           AS scientificName,
   'Animalia'                            AS kingdom
